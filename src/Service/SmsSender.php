945058c1166c5e26a39a52ecfc7c53eb167e6def
farahBY <farah.benyedder@esprit.tn>
2025-04-28 00:21:48 +0100

    SMS

--- src/Service/SmsSender.php
+++ src/Service/SmsSender.php
@@ -1,42 +1,4 @@
 <?php
-//
-//namespace App\Service;
-//
-//use Twilio\Rest\Client;
-//use Twilio\Exceptions\TwilioException;
-//
-//class SmsSender
-//{
-//    private Client $client;
-//    private string $from;
-//
-//    public function __construct(string $twilioSid, string $twilioToken, string $from)
-//    {
-//        $this->client = new Client($twilioSid, $twilioToken);
-//        $this->from = $from;
-//    }
-//
-//    /**
-//     * Envoie un SMS via l'API Twilio.
-//     *
-//     * @param string $to      Numéro de téléphone du destinataire au format E.164 (ex: +216xxxxxxxx)
-//     * @param string $message Contenu du message SMS
-//     * @throws TwilioException En cas d'erreur lors de l'envoi du SMS
-//     */
-//    public function send(string $to, string $message): void
-//    {
-//        try {
-//            $this->client->messages->create($to, [
-//                'from' => $this->from,
-//                'body' => $message,
-//            ]);
-//        } catch (TwilioException $e) {
-//            throw new TwilioException('Erreur lors de l\'envoi du SMS : ' . $e->getMessage(), $e->getCode(), $e);
-//        }
-//    }
-//}
-
-
 
 namespace App\Service;
 
@@ -45,27 +7,30 @@ use Twilio\Exceptions\TwilioException;
 
 class SmsSender
 {
-    private Client $client;
-    private string $from;
+    private $client;
+    private $fromNumber;
 
-    public function __construct()
+    public function __construct(string $sid, string $authToken, string $fromNumber)
     {
-        $sid   = getenv('TWILIO_SID');
-        $token = getenv('TWILIO_AUTH_TOKEN');
-        $this->from = getenv('TWILIO_PHONE_NUMBER');
-
-        $this->client = new Client($sid, $token);
+        $this->client = new Client($sid, $authToken);
+        $this->fromNumber = $fromNumber;
     }
 
-    public function send(string $to, string $message): void
+    public function sendSms(string $toNumber, string $message): bool
     {
         try {
-            $this->client->messages->create($to, [
-                'from' => $this->from,
-                'body' => $message,
-            ]);
+            $this->client->messages->create(
+                $toNumber, // Numéro au format international, ex: +33612345678
+                [
+                    'from' => $this->fromNumber,
+                    'body' => $message,
+                ]
+            );
+            return true;
         } catch (TwilioException $e) {
-            throw new TwilioException('Erreur SMS : '.$e->getMessage(), $e->getCode(), $e);
+            // Log l'erreur (utilise un logger si configuré)
+            error_log('Erreur Twilio: ' . $e->getMessage());
+            return false;
         }
     }
 }
