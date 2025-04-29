7e61498ea0bc1306f1821fc9b8957406a00f9729
houssem <95879355+housssemm@users.noreply.github.com>
2025-04-29 02:37:45 +0100

    version 5

--- config/bundles.php
+++ config/bundles.php
@@ -9,10 +9,5 @@ return [
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-
-
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-
-
-
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
