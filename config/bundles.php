b6b31a255acf6fab476d66f0c9e9dec67313e63e
houssem <95879355+housssemm@users.noreply.github.com>
2025-06-09 15:38:52 +0100

    test live

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
